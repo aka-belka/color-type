a82a88c20a07fbd7f9c7eff90d2fc51d0424e6cd
Тухватуллина Азалия <azaleatukhvat@gmail.com>
2026-02-25 21:58:43 +0300

    исправить модель юзера (без юзнейма)

--- back/app/models/user.py
+++ back/app/models/user.py
@@ -4,19 +4,18 @@ from app.core.database import Base
 
 class User(Base):
     __tablename__ = "users"
-
+    
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False) # email как логин
     hashed_password = Column(String, nullable=False)
-    first_name = Column(String)
-    last_name = Column(String)
-    gender = Column(String)
+    first_name = Column(String, nullable=False)  
+    last_name = Column(String, nullable=False)   
+    phone = Column(String, nullable=False)       
+    gender = Column(String, nullable=False)      
     role = Column(String, default="user")
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
-
+    
     def __repr__(self):
         return f"<User {self.email}>"
-    
